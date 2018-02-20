c3fbddf17c930add8258ef0aa6e09b33d4fde908
Niels Heltner <niels.heltner@hotmail.com>
2018-02-20 01:08:10 +0100

    run er nu en del mere interaktiv

--- run.py
+++ run.py
@@ -2,15 +2,10 @@ from configobj import ConfigObj
 from sys import executable
 from subprocess import Popen, CREATE_NEW_CONSOLE
 import psutil
+import time
 
 SERVICES_DIR = 'services/'
 SERVICES_CONFIG = 'services.ini'
-
-
-def load_dependencies():
-    config = ConfigObj(SERVICES_CONFIG)
-    for service in config:
-        launch_service(service, config)
 
 
 def launch_service(service, config):
@@ -21,12 +16,11 @@ def launch_service(service, config):
     else:
         instances = 1
 
-    print('Checking {} service...'.format(service))
     amount_of_instances = check_port(int(port))
     if amount_of_instances < instances:
         instance_difference = instances - amount_of_instances
-        print("Only {} instances of {} service are running, starting {} more at port {}".format(
-            amount_of_instances, service, instance_difference, port))
+        print("Only {} instances of {} service are running, starting {} more of version {} at port {}".format(
+            amount_of_instances, service, instance_difference, version, port))
         for i in range(instance_difference):
             Popen([executable, SERVICES_DIR + service + '/' + service + '.py',
                    '-p', port, '-v', version], creationflags=CREATE_NEW_CONSOLE)
@@ -44,5 +38,46 @@ def check_port(port_to_check):
     return amount_of_instances
 
 
+def gather_pids(port_to_check):
+    pids = []
+    for connection in psutil.net_connections('inet'):
+        (ip, port) = connection.laddr
+        if ip == '127.0.0.1' and port == port_to_check:
+            pids.append(connection.pid)
+    return pids
+
+
+def run(service):
+    config = ConfigObj(SERVICES_CONFIG)
+    print('Starting {} service...'.format(service))
+    if service in config:
+        launch_service(service, config)
+    elif service == 'all':
+        for service_section in config:
+            launch_service(service_section, config)
+
+
+def upgrade(service):
+    config = ConfigObj(SERVICES_CONFIG)
+    if service in config:
+        print('Starting upgrade of {} service...'.format(service))
+        port = int(config[service]['port'])
+        amount_of_instances = check_port(port)
+        count = 0
+        for pid in gather_pids(port):
+            count += 1
+            print('Upgrading service with PID {} ({}/{})'.format(pid,
+                                                                 count, amount_of_instances))
+            psutil.Process(pid).terminate()
+            time.sleep(1)
+            launch_service(service, config)
+
+
 if __name__ == '__main__':
-    load_dependencies()
+    while True:
+        method_name, arg = input().split()
+        method = locals().get(method_name)
+        if not method:
+            print('Method {} does not exist'.format(method_name))
+        else:
+            method(arg)
