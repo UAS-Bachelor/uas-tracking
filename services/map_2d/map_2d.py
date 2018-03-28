e8398c3a9b2d9f83df285c24ac22a2da0251037f
Niels Heltner <niels.heltner@hotmail.com>
2018-03-28 17:45:59 +0200

    Added new features to the 2d map
    Changed the color of the route
    Added dots for each 'breakpoint' on the route
    Added mouseover tooltip for the route and breakpoints, that shows
    coordinates, time and duration

--- services/map_2d/map_2d.py
+++ services/map_2d/map_2d.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request, json
 import requests
 import sys
 import argparse
+import time
 from os import system
 
 app = Flask(__name__)
@@ -16,9 +17,14 @@ def map():
 def map_2d_with_params(id, start_time, end_time):
     try:
         drone_route_list = json.loads(requests.get('http://127.0.0.1:5005/' + id + '/' + start_time + '/' + end_time).text)
+        route_duration = epoch_to_time(drone_route_list[-1]['time'] - drone_route_list[0]['time'])
     except requests.exceptions.ConnectionError:
-        return '2D Map service unavailable'
-    return render_template('map.html', drone_route_list=drone_route_list)
+        return 'Drone information service unavailable'
+    return render_template('map.html', drone_route_list=drone_route_list, route_duration=route_duration)
+
+
+def epoch_to_time(epoch):
+    return time.strftime('%H:%M:%S', time.gmtime(epoch))
 
 
 if __name__ == '__main__':
@@ -35,4 +41,4 @@ if __name__ == '__main__':
     print('Running {} service version {}'.format(args.prog, args.version))
     system('title {} service version {} on {}:{}'.format(
         args.prog, args.version, args.address, args.port))
-    app.run(host=args.address, port=args.port, debug=False)
+    app.run(host=args.address, port=args.port, debug=True)
--- services/map_2d/static/map.js
+++ services/map_2d/static/map.js
@@ -10,36 +10,113 @@ map.addLayer(new ol.layer.Tile({
     source: new ol.source.OSM()
 }));
 var coordinates = []
-for (var i = 0; i < drone_routes.length; i++) {
-    coordinates.push([drone_routes[i]['lon'], drone_routes[i]['lat']])
+for (var i = 0; i < droneRoutes.length; i++) {
+    coordinates.push([droneRoutes[i]['lon'], droneRoutes[i]['lat']])
 }
 
-var coords = [[10.3, 55.2], [10.4, 55.5], [10.3, 55.9]];
+//var coords = [[10.3, 55.2], [10.4, 55.5], [10.3, 55.9]];
+
+var lineWidth = 7;
+if(
+    (screen.width <= 640) || 
+    (window.matchMedia && 
+     window.matchMedia('only screen and (max-width: 640px)').matches
+    )
+  ){
+   lineWidth = 12;
+}
 
 
 var lineString = new ol.geom.LineString(coordinates);
 lineString.transform('EPSG:4326', 'EPSG:3857');
 
+var styles = [
+    new ol.style.Style({ //lineStyle
+        stroke: new ol.style.Stroke({
+            color: '#00b3fd',
+            width: lineWidth
+        }), 
+        zIndex: 1
+    }), 
+    new ol.style.Style({ //lineBorderStyle
+        stroke: new ol.style.Stroke({
+            color: '#2865a2',
+            width: lineWidth + (lineWidth / 3)
+        }), 
+        zIndex: 0
+    }), 
+    new ol.style.Style({
+        image: new ol.style.Circle({
+            radius: (lineWidth / 2), 
+            fill: new ol.style.Fill({
+                color: '#f5f5f5'
+            }), 
+            stroke: new ol.style.Stroke({
+              color: '#2865a2',
+              width: (lineWidth / 3)
+          })
+        }), 
+        zIndex: 2
+      })
+];
 
-var lineStyle = new ol.style.Style({
-    stroke: new ol.style.Stroke({
-        color: '#ffcc33',
-        width: 10
+var routeFeatures = [
+    new ol.Feature({
+        geometry: lineString,
+        name: 'Rute<br>start ' + droneRoutes[0]['time_stamp'] + '<br>slut &nbsp;' + droneRoutes[droneRoutes.length - 1]['time_stamp'] + '<br>varighed ' + routeDuration
     })
+];
+
+var pointFeatures = []
+
+var geometry = routeFeatures[0].getGeometry()
+var i = 0;
+geometry.forEachSegment(function(start, end) {
+    var droneRoute = droneRoutes[i];
+    pointFeatures.push(new ol.Feature({
+        geometry: new ol.geom.Point(start), 
+        name: 'Punkt<br>lat ' + droneRoute['lat'] + '<br>lon ' + droneRoute['lon'] + '<br>tid ' + droneRoute['time_stamp']
+    }))
+    i++;
 });
 
 var source = new ol.source.Vector({
-	features: [new ol.Feature({
-        geometry: lineString,
-        name: 'Line'
-    })]
+	features: routeFeatures.concat(pointFeatures)
 });
 var vector = new ol.layer.Vector({
 	source: source,
-	style: [lineStyle]
+	style: styles
 });
 
 map.addLayer(vector)
+
+
+console.log(window.screen.availWidth)
+console.log(window.screen.availHeight)
+
+
+var tooltip = document.getElementById('tooltip');
+var overlay = new ol.Overlay({
+  element: tooltip,
+  offset: [10, 10],
+  positioning: 'top-left'
+});
+map.addOverlay(overlay);
+
+function displayTooltip(evt) {
+  var pixel = evt.pixel;
+  var feature = map.forEachFeatureAtPixel(pixel, function(feature) {
+    return feature;
+  });
+  tooltip.style.display = feature ? '' : 'none';
+  if (feature) {
+    overlay.setPosition(evt.coordinate);
+    tooltip.innerHTML = feature.get('name');
+  }
+};
+
+map.on('pointermove', displayTooltip);
+
 
 
 //console.log($('#my-data').data());
