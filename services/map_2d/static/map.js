fdaf104351330f04bff3ce1c1b4f51bd0c83eed1
Niels Heltner <niels.heltner@hotmail.com>
2018-03-28 19:51:09 +0200

    Tooltip formatting

--- services/map_2d/static/map.js
+++ services/map_2d/static/map.js
@@ -1,8 +1,8 @@
 var map = new ol.Map({
-    target: 'map', 
+    target: 'map',
     view: new ol.View({
         projection: 'EPSG:3857',
-        center: ol.proj.transform([10.4, 55.9],  'EPSG:4326', 'EPSG:3857'),
+        center: ol.proj.transform([10.4, 55.9], 'EPSG:4326', 'EPSG:3857'),
         zoom: 7
     })
 });
@@ -11,19 +11,19 @@ map.addLayer(new ol.layer.Tile({
 }));
 var coordinates = []
 for (var i = 0; i < droneRoutes.length; i++) {
-    coordinates.push([droneRoutes[i]['lon'], droneRoutes[i]['lat']])
+    coordinates.push([droneRoutes[i]['lon'], droneRoutes[i]['lat']]);
 }
 
 //var coords = [[10.3, 55.2], [10.4, 55.5], [10.3, 55.9]];
 
 var lineWidth = 7;
-if(
-    (screen.width <= 640) || 
-    (window.matchMedia && 
-     window.matchMedia('only screen and (max-width: 640px)').matches
+if (
+    (screen.width <= 640) ||
+    (window.matchMedia &&
+        window.matchMedia('only screen and (max-width: 640px)').matches
     )
-  ){
-   lineWidth = 10;
+) {
+    lineWidth = 10;
 }
 
 
@@ -35,60 +35,71 @@ var styles = [
         stroke: new ol.style.Stroke({
             color: '#00b3fd',
             width: lineWidth
-        }), 
+        }),
         zIndex: 1
-    }), 
+    }),
     new ol.style.Style({ //lineBorderStyle
         stroke: new ol.style.Stroke({
             color: '#2865a2',
             width: lineWidth + (lineWidth / 3)
-        }), 
+        }),
         zIndex: 0
-    }), 
+    }),
     new ol.style.Style({ //pointStyle
         image: new ol.style.Circle({
-            radius: (lineWidth / 2), 
+            radius: (lineWidth / 2),
             fill: new ol.style.Fill({
                 color: '#f5f5f5'
-            }), 
+            }),
             stroke: new ol.style.Stroke({
-              color: '#2865a2',
-              width: (lineWidth / 3)
-          })
-        }), 
+                color: '#2865a2',
+                width: (lineWidth / 3)
+            })
+        }),
         zIndex: 2
-      })
-];
-
-var routeFeatures = [
-    new ol.Feature({
-        geometry: lineString,
-        name: 'Rute<br>start ' + droneRoutes[0]['time_stamp'] + '<br>slut &nbsp;' + droneRoutes[droneRoutes.length - 1]['time_stamp'] + '<br>varighed ' + routeDuration
     })
 ];
 
-var pointFeatures = []
+var startTime = droneRoutes[0]['time_stamp'];
+var endTime = droneRoutes[droneRoutes.length - 1]['time_stamp'];
+var routeFeatures = [
+    new ol.Feature({
+        geometry: lineString,
+        html: '<b>Rute</b><br>' + 
+                '<b>Start:</b> ' + startTime + '<br>' + 
+                '<b>Slut:</b> ' + endTime + '<br>' + 
+                '<b>Varighed:</b> ' + routeDuration
+    })
+];
 
-var geometry = routeFeatures[0].getGeometry()
+var pointFeatures = [];
+
+var geometry = routeFeatures[0].getGeometry();
 var i = 0;
-geometry.forEachSegment(function(start, end) {
+geometry.forEachSegment(function (start, end) {
     var droneRoute = droneRoutes[i];
+    var lat = droneRoute['lat'];
+    var lon = droneRoute['lon'];
+    var time = droneRoute['time_stamp'];
     pointFeatures.push(new ol.Feature({
-        geometry: new ol.geom.Point(start), 
-        name: 'Punkt<br>lat ' + droneRoute['lat'] + '<br>lon ' + droneRoute['lon'] + '<br>tid ' + droneRoute['time_stamp']
-    }))
+        geometry: new ol.geom.Point(start),
+        html: '<b>Punkt</b><br>' + 
+                '<b>Latitude:</b> ' + lat + '<br>' + 
+                '<b>Longitude:</b> ' + lon + '<br>' + 
+                '<b>Tid:</b> ' + time
+    }));
     i++;
 });
 
 var source = new ol.source.Vector({
-	features: routeFeatures.concat(pointFeatures)
+    features: routeFeatures.concat(pointFeatures)
 });
 var vector = new ol.layer.Vector({
-	source: source,
-	style: styles
+    source: source,
+    style: styles
 });
 
-map.addLayer(vector)
+map.addLayer(vector);
 
 
 
@@ -96,22 +107,22 @@ map.addLayer(vector)
 
 var tooltip = document.getElementById('tooltip');
 var overlay = new ol.Overlay({
-  element: tooltip,
-  offset: [10, 10],
-  positioning: 'top-left'
+    element: tooltip,
+    offset: [10, 10],
+    positioning: 'top-left'
 });
 map.addOverlay(overlay);
 
 function displayTooltip(evt) {
-  var pixel = evt.pixel;
-  var feature = map.forEachFeatureAtPixel(pixel, function(feature) {
-    return feature;
-  });
-  tooltip.style.display = feature ? '' : 'none';
-  if (feature) {
-    overlay.setPosition(evt.coordinate);
-    tooltip.innerHTML = feature.get('name');
-  }
+    var pixel = evt.pixel;
+    var feature = map.forEachFeatureAtPixel(pixel, function (feature) {
+        return feature;
+    });
+    tooltip.style.display = feature ? '' : 'none';
+    if (feature) {
+        overlay.setPosition(evt.coordinate);
+        tooltip.innerHTML = feature.get('html');
+    }
 };
 
 map.on('pointermove', displayTooltip);
