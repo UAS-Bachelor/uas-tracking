ac9f972d9e3e13aced9f5397e5811dc9ce1494b6
larschristensen20 <lars.christensen20@gmail.com>
2018-04-16 22:05:48 +0200

    nofly information service

---
+++ services/nofly_information/nofly_information.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify, Response, send_from_directory
+import requests
+import json
+import argparse
+import sys
+import os
+
+
+app = Flask(__name__, static_folder='static')
+
+
+@app.route('/zones')
+def get_nofly_zones_list():
+    return send_from_directory(app.static_folder, 'drone_nofly_dk.kml', mimetype='text/xml') 
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--address', type=str, default='127.0.0.1',
+                        help='specify which host to run this service on')
+    parser.add_argument('-p', '--port', type=int, default=5004,
+                        help='specify which port to run this service on')
+    parser.add_argument('-v', '--version', type=float, default=0,
+                        help='specify which version of the service this is')
+    args = parser.parse_args()
+    args.prog = sys.argv[0].split('/')[-1].split('.')[0]
+
+    print('Running {} service version {}'.format(args.prog, args.version))
+    os.system('title {} service version {} on {}:{}'.format(
+        args.prog, args.version, args.address, args.port))
+    app.run(host=args.address, port=args.port, debug=True, threaded=True)
