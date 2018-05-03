5f230ee3c547707f18c7281fa52788bdc821a407
larschristensen20 <lars.christensen20@gmail.com>
2018-05-03 12:09:10 +0200

    added collapsible false, per the docs should be set false if OSM source

--- services/map_2d/static/js/map.js
+++ services/map_2d/static/js/map.js
@@ -24,6 +24,10 @@ if (
 function initMap() {
     map = new ol.Map({
         target: 'map',
+        controls: ol.control.defaults({
+            attributionOptions: {
+              collapsible: false
+            }}),
         view: new ol.View({
             projection: 'EPSG:3857',
             center: ol.proj.transform([10.4, 55.9], 'EPSG:4326', 'EPSG:3857'),
