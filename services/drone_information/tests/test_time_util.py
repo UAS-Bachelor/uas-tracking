b0308d82e751743597b64adbb884760e24cef088
Niels Heltner <niels.heltner@hotmail.com>
2018-05-10 21:39:55 +0200

    added single quotes instead of double ones

--- services/drone_information/tests/test_time_util.py
+++ services/drone_information/tests/test_time_util.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 import time_util
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope='module')
 def epoch_time():
     yield 1517481030#01 Feb 2018, 10:30:30
 
