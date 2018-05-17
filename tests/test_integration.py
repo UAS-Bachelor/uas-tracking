61a423ca8993c8d6f5ea3b7411074fb7e4419d5e
Niels Heltner <niels.heltner@hotmail.com>
2018-05-17 21:12:41 +0200

    refactored tests to 'api_gateway'

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,16 +6,16 @@ import json
 import time
 from multiprocessing import Process
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from services.app import app
+from services.api_gateway import api_gateway
 from services.drone_information import drone_information
 from scripts.util import config
 
 
 @pytest.fixture
 def app_client():
-    app.app.config['TESTING'] = True
-    app.config = json.load(open(app.__services_config_file))
-    app_client = app.app.test_client()
+    api_gateway.app.config['TESTING'] = True
+    api_gateway.config = json.load(open(api_gateway.__services_config_file))
+    app_client = api_gateway.app.test_client()
     yield app_client
 
 
@@ -43,7 +43,7 @@ def test_different_ports_for_same_service(app_client):
     new_port = 6001
     server_2 = start_drone_information_server(new_port)
     
-    app.config = {
+    api_gateway.config = {
         'drone_information': {
             'host': '127.0.0.1', 
             'port': new_port + 1
@@ -53,7 +53,7 @@ def test_different_ports_for_same_service(app_client):
     assert response.status_code == 503
     assert response.data.decode().strip() == 'Drone information service unavailable'
 
-    app.config = {
+    api_gateway.config = {
         'drone_information': {
             'host': '127.0.0.1', 
             'port': new_port
